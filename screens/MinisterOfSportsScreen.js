cbf33b51a8e165220f0c72fb31b2b793e59c8906
Zzero202 <ahmedfaisaly2000@gmail.com>
2024-06-26 04:06:13 +0300

    adding scroll

--- screens/MinisterOfSportsScreen.js
+++ screens/MinisterOfSportsScreen.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import Sidebar from '../components/Layout/Sidebar/Sidebar';
 import MainContent from '../components/Layout/MainContent/MainContent';
 function MinisterOfSportsScreen() {
   return (
     <View style={styles.mainPage}>
-      <Sidebar />
-      <MainContent />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.scrollViewContent}>
+        <Sidebar />
+        <MainContent />
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   mainPage: {
+    marginTop: 25,
+  },
+  scrollViewContent: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginTop: 25,
     backgroundColor: '#F7F8F9',
   },
 });
